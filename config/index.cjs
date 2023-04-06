afeb5dc877af23b71020a29abcb6b955869cac4a
WWH012 <130033574+WWH012@users.noreply.github.com>
2023-04-06 17:42:52 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oiYkV53iNtHl0O003GRYDods1rzs',
+      id: 'oiYkV58gVncpqfN8Bdy4K_MzbYzA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '6X1SrUoJ6HzqeUnFuV7imZGTtelv4yOQw8JhZiXV3KQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
